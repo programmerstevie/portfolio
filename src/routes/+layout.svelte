53abb7aca8dc660ec2a1345b7e4cde86d6a914b6
steven <stevensan0606@gmail.com>
2024-03-15 15:47:53 -0400

    changed title

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -9,5 +9,7 @@
 	<link
 		href="https://fonts.googleapis.com/css2?family=Roboto+Mono:ital,wght@0,100..700;1,100..700&family=Roboto:ital,wght@0,100;0,300;0,400;0,500;0,700;0,900;1,100;1,300;1,400;1,500;1,700;1,900&display=swap"
 		rel="stylesheet"
-	/></svelte:head
+	/>
+  <title>Hello I'm Stevie.</title>
+  </svelte:head
 ><slot />
