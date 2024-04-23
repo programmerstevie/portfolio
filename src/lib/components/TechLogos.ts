31408fb5a2bcdbf0933b944231204662decff29a
steven <stevensan0606@gmail.com>
2024-04-23 19:18:37 -0400

    added pictures and one more colored tech stack logo

--- src/lib/components/TechLogos.ts
+++ src/lib/components/TechLogos.ts
@@ -8,6 +8,7 @@ import tech_logo_color_svelte from '$lib/assets/icons/tech/color/svg/SVELTE.svg'
 import tech_logo_color_angular from '$lib/assets/icons/tech/color/svg/ANGULAR.svg';
 import tech_logo_color_sass from '$lib/assets/icons/tech/color/svg/SASS.svg';
 import tech_logo_color_tailwind from '$lib/assets/icons/tech/color/svg/TAILWIND.svg';
+import tech_logo_color_electron from '$lib/assets/icons/tech/color/svg/ELECTRON.svg';
 
 import tech_logo_mono_git from '$lib/assets/icons/tech/mono/svg/GIT_ICON.svg';
 import tech_logo_mono_js from '$lib/assets/icons/tech/mono/svg/JAVASCRIPT_ICON.svg';
@@ -25,7 +26,25 @@ import tech_logo_mono_python from '$lib/assets/icons/tech/mono/svg/PYTHON_ICON.s
 import tech_logo_mono_aws from '$lib/assets/icons/tech/mono/svg/AWS_ICON.svg';
 import tech_logo_mono_electron from '$lib/assets/icons/tech/mono/svg/ELECTRON_ICON.svg';
 
-export const color_logo = {
+export enum TechName {
+	GIT = 'git',
+	HTML = 'html5',
+	CSS = 'css3',
+	JS = 'js',
+	TS = 'ts',
+	NODEJS = 'nodejs',
+	REACT = 'react',
+	SVELTE = 'svelte',
+	NEXTJS = 'nextjs',
+	ANGULAR = 'angular',
+	TAILWIND = 'tailwind',
+	SASS = 'sass',
+	PYTHON = 'python',
+	AWS = 'aws',
+	ELECTRON = 'electron'
+}
+
+export const color_logo: Partial<Record<TechName, string>> = {
 	js: tech_logo_color_js,
 	ts: tech_logo_color_ts,
 	html5: tech_logo_color_html5,
@@ -35,11 +54,12 @@ export const color_logo = {
 	svelte: tech_logo_color_svelte,
 	angular: tech_logo_color_angular,
 	sass: tech_logo_color_sass,
-	tailwind: tech_logo_color_tailwind
+	tailwind: tech_logo_color_tailwind,
+  electron: tech_logo_color_electron
 };
 
-export const mono_logo = {
-  git: tech_logo_mono_git,
+export const mono_logo: Record<TechName, string> = {
+	git: tech_logo_mono_git,
 	js: tech_logo_mono_js,
 	html5: tech_logo_mono_html5,
 	css3: tech_logo_mono_css3,
@@ -53,5 +73,5 @@ export const mono_logo = {
 	sass: tech_logo_mono_sass,
 	python: tech_logo_mono_python,
 	aws: tech_logo_mono_aws,
-	electron: tech_logo_mono_electron,
-}
+	electron: tech_logo_mono_electron
+};
