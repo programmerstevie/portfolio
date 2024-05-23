a862eef29fca610343f15ccefd1d7d77aaed8099
steven <stevensan0606@gmail.com>
2024-05-22 20:00:48 -0400

    small view not supported yet

---
+++ src/lib/components/mobile/ComingSoon/ComingSoon.svelte
@@ -0,0 +1,59 @@
+<script lang="ts">
+	import icon_underline_src from '$lib/assets/icons/general/png/underline.png';
+</script>
+
+<section class="mainsection">
+	<header>
+		<h1>Small View Not Supported Yet!</h1>
+		<img src={icon_underline_src} alt="" />
+	</header>
+	<p class="p1">Steven's portfolio is not available on smaller screen sizes!</p>
+	<p class="p2">Please use a full computer screen to view my portfolio!</p>
+</section>
+
+<style lang="scss">
+	.mainsection {
+		width: 100vw;
+		height: 100vh;
+		background-image: var(--color-pf-landing-gradient);
+
+		color: var(--color-pf-light-gray);
+		text-align: center;
+
+		display: flex;
+		flex-direction: column;
+		align-items: center;
+		justify-content: center;
+    z-index: 0;
+
+		header {
+			position: relative;
+      z-index: 0;
+			h1 {
+				font-size: 6vw;
+				font-weight: bold;
+				color: var(--color-pf-primary);
+			}
+			img {
+				position: absolute;
+        right: 0;
+        top: 5vw;
+        height: 12vw;
+        width: 50vw;
+        z-index: -1;
+			}
+		}
+
+    p {
+      z-index: 1;
+      font-size: 5vw;
+    }
+
+		.p1 {
+			padding: 14vw 20px 0px 20px;
+		}
+		.p2 {
+			padding: 2vw 20px 0px 20px;
+		}
+	}
+</style>
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,15 +1,40 @@
 <script>
+	// Desktop
 	import Landing from '$lib/components/sections/Landing';
 	import AttributesBar from '$lib/components/AttributesBar';
 	import AboutMe from '$lib/components/sections/AboutMe';
 	import ToolingBar from '$lib/components/ToolingBar';
 	import Portfolio from '$lib/components/sections/Portfolio';
 	import Contact from '$lib/components/sections/Contact';
+
+	// Mobile
+	import ComingSoon from '$lib/components/mobile/ComingSoon';
 </script>
 
-<Landing />
-<AttributesBar />
-<AboutMe />
-<ToolingBar />
-<Portfolio />
-<Contact />
+<div class="desktop-view">
+	<Landing />
+	<AttributesBar />
+	<AboutMe />
+	<ToolingBar />
+	<Portfolio />
+	<Contact />
+</div>
+<div class="mobile-view">
+	<ComingSoon />
+</div>
+
+<style lang="scss">
+	@media (max-width: 1200px) {
+		.desktop-view {
+			display: none;
+		}
+		.mobile-view {
+			display: default;
+		}
+	}
+	@media (min-width: 1201px) {
+		.mobile-view {
+			display: none;
+		}
+	}
+</style>
